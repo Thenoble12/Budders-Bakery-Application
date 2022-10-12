972d39a6be04100473e1f1b36e0932b26af25575
Elijah Noble <elijahnobledev@gmail.com>
2022-10-12 12:52:30 -0700

    Fixed Deployment issues

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,17 @@
-class PaymentController < ApplicationController
+class PaymentsController < ApplicationController
+    def index
+        payments = Payment.all
+        render json: payments
+    end
+
+    def show
+        payment = Payment.find(params[:id])
+        render json: payment, serializer: PaymentSerializer
+    end
+
+    
+    def create
+        payment = Payment.create(payment_params)
+        render json: payment, status: :created        
+    end
 end
